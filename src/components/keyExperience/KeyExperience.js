27610ba9645bd96f4c0ab23482cbe8a6d2a39077
sathiyendren <sathiyendren@gmail.com>
2023-06-30 08:22:47 -0400

    latest resume 2023

--- src/components/keyExperience/KeyExperience.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable react/prop-types */
-import "./KeyExperience.css";
-
-export default function KeyExperience(props) {
-  return <div className="key-experience">{props.name}</div>;
-}
--- src/components/technology/Technology.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable react/prop-types */
-import "./Technology.css";
-
-export default function Technology(props) {
-  return <div className="technology">{props.name}</div>;
-}
